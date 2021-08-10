cbd2c58cdecb9f866f70253a53e64839ee200801
Jave Paid <jave.paid@obf.ateneo.edu>
2021-08-10 21:50:44 +0800

    Add pekofy with input test

---
+++ tests/test_pekofy.py
@@ -0,0 +1,18 @@
+from src import pekofy
+import unittest
+
+class TestPeko(unittest.TestCase):
+    def test_with_input(self):
+        puncs = ['.', '!', '?', '. ', '! ', '? ', ' ']
+        for i in puncs:
+            self.assertEqual(pekofy('hello world' + i), 
+                            'hello world {}{}'.format('peko', i))
+
+if __name__ == '__main__':
+    unittest.main()
+
+'''
+class TestSample(unittest.TestCase):
+    def test_sample(self):
+        self.assertEqual(1,1); <---- bruh XDDDDDD
+'''
