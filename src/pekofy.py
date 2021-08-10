9b5babfc3a57455356a7a43fbbd221408d6d1870
Ian Palabasan <ian.palabasan@eee.upd.edu.ph>
2021-08-10 23:35:40 +0900

    Fixed edge case where sentence ends with a space.

--- src/pekofy.py
+++ src/pekofy.py
@@ -16,7 +16,16 @@ def pekofy(words):
     orig_sentences = str(words)
     if orig_sentences != "":
         temp_list = re.split("([.?!])", orig_sentences)
-        pekofied_list = [i+' peko' if i not in punc else i for i in temp_list]
+        pekofied_list = []
+        for i in temp_list:
+            if i in punc:
+                pekofied_list.append(i)
+            else:
+                a=-1
+                while i[a]==' ':
+                    a=a-1
+                a=-(a+1)
+                pekofied_list.append(i.strip(" ")+" peko"+" "*a)
         peko = ''.join(pekofied_list)
         return peko
     else:
