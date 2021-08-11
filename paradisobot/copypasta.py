291b05f016ec9c90c452b9e4fa6afa2e9555db2f
firwithfeedback <4010706+firwithfeedback@users.noreply.github.com>
2021-08-12 00:30:03 +0900

    Fixed issue with relative paths

--- paradisobot/copypasta.py
+++ paradisobot/copypasta.py
@@ -1,3 +1,5 @@
+import os
+
 def copypasta(cp_name):
     """
     Returns a copypasta from a list of copypastas when user inputs a copypasta name. 
@@ -12,7 +14,8 @@ def copypasta(cp_name):
     """
 
     try:
-        cp = open('copypastas/{}.txt'.format(cp_name.strip()), 'r')
+        cp = open(os.path.join(os.path.dirname(__file__), 'copypastas/{}.txt'.format(cp_name.strip())), 'r')
+
         return cp.read()
     except IOError:
         return "Copypasta does not exist"
