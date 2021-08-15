1309f79159b69872848418d8849689eaed0d7bbc
firwithfeedback <4010706+firwithfeedback@users.noreply.github.com>
2021-08-16 02:35:30 +0900

    Add cp fallback; Add ability to change prefix.

--- paradisobot-instance.py
+++ paradisobot-instance.py
@@ -4,23 +4,29 @@ import os
 
 client = discord.Client()
 
+prefix = '/para'
+
 @client.event
 async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.startswith('/para hello'):
+    if message.content.startswith(prefix+' hello'):
         await message.channel.send('Hello!')
-    if message.content.startswith('/para cp'):
+    elif message.content.startswith(prefix+' cp'):
         keyword = message.content.split(' ')[2]
         await message.channel.send(paradisobot.copypasta(keyword))
-    if message.content.startswith('/para pekofy'):
+    elif message.content.startswith(prefix+' pekofy'):
         if message.reference is None:
             await message.channel.send(paradisobot.pekofy(""))
         else :
             channelr = client.get_channel(message.reference.channel_id)
             messager = await channelr.fetch_message(message.reference.message_id)
             await message.channel.send(paradisobot.pekofy(messager.content))
+    else: # this also implies /para help, help is implemented as a copypasta
+        if message.content.startswith(prefix):
+            keyword = message.content.split(' ')[1]
+            await message.channel.send(paradisobot.copypasta(keyword))
 
 if os.environ.get("DISCORD_API_KEY"):
     client.run(os.environ.get("DISCORD_API_KEY"))
