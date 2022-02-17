355299940366f4e4a077f7e1734d6fcb328354fd
javepaid <paidjavegerard@gmail.com>
2022-02-17 15:52:11 +0800

    Add temperature readout

--- paradisobot/weather.py
+++ paradisobot/weather.py
@@ -1,7 +1,7 @@
 from pyowm import OWM
 from pyowm.commons import exceptions 
 
-with open('owm_api.txt') as owm_api:
+with open('owm_api.txt', 'r') as owm_api:
     api_key = owm_api.read().strip()
     owm = OWM(api_key)
 
@@ -11,11 +11,15 @@ def weather(city):
     try:
         obs = mgr.weather_at_place(city)
         w = obs.weather
-        location = 'Weather at {}, {}: '.format(obs.location.name, obs.location.country)
-        return location
+        location = 'Weather at {}, {} right now: '.format(obs.location.name, obs.location.country)
+        ctemp = w.temperature('celsius')
+        ftemp = w.temperature('fahrenheit')
+        temp = ('Temperature: {}\u00b0C / {}\u00b0F ({}\u00b0C max /' + 
+                '{}\u00b0 min)').format(ctemp['temp'], ftemp['temp'], ctemp['temp_max'], ctemp['temp_min'])
+        return location + '\n' + temp
     except exceptions.NotFoundError:
         return "Location not found"
 
-print(weather('adnKmmeci'))
+print(weather('manila'))
 # print(w.detailed_status, w.temperature('celsius'), w.wind(), obs.location.name, obs.location.country)
 
